be8dc6ceb23e78da0fda27799af4e5b0c76ac61b
Leidiane Borges <leidianeborges21@gmail.com>
2018-02-20 02:04:42 -0300

    primeiro commit da api de clientes

---
+++ database/migrations/2018_02_19_195909_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('dt_nascimento');
+            $table->string('cpf');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->string('endereco');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
